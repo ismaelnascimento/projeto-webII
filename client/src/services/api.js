077db8bb528e1c5dfa25f880b8e9173ba69b1622
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2025-10-23 20:50:48 +0000

    editando navbar

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "https://glorious-succotash-rw9r9grjqv5cwqjr-3001.app.github.dev"
+    // baseURL: "http://localhost:3001"
 });
 
 export default api;
